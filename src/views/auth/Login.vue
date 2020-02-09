41c1d217ddef7e788f89c651cdf5b620080a6294
Quan Nguyen <quan.nguyen2@asiantech.vn>
2020-02-09 16:12:17 +0700

    init project

---
+++ src/views/auth/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <div class="login">
+      <h1>Quan</h1>
+    </div>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  name: 'Login',
+  components: {
+    //
+  }
+}
+</script>
